2fb67a4f2c545058d311b0ea72cc5c089b1dd009
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-06 16:19:58 +0100

    created 2 extracted components for buttons in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import Article from "@/components/Article";
 import Layout from "@/components/Layout";
+import BetterButton from "../components/BetterButton";
 
 const Home = () => {
+  const textVar = "Text";
+
   return (
     <Layout>
+      <BetterButton variant="primary">
+        <span>Change the {textVar}</span>
+      </BetterButton>
+      <BetterButton variant="secondary">Another One</BetterButton>
       <Article title="Today, nothing." author="Mary" />
+      <h1>Hello!</h1>
     </Layout>
   );
 };
