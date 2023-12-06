26bb98912fa80a2adfea491a4cb23fb0724cb100
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-06 21:53:30 +0100

    created setToken state (useEffect) and if token exists return {children}, and if token is null return "not logged in" ---Now in incognito browser I can not keep logged in---

--- src/components/ProtectedPage.tsx
+++ src/components/ProtectedPage.tsx
@@ -1,20 +1,36 @@
 import Link from "next/link";
 import Layout from "./Layout";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 interface ProtectedPageProps {
   children: ReactNode;
 }
 
 const ProtectedPage = ({ children }: ProtectedPageProps) => {
-  return (
-    <div>
-      <Layout>
-        <h1>🔒 NOT LOGGED IN!!</h1>
-        <Link href="/login">Login Here</Link>
-      </Layout>
-    </div>
-  );
+  const [token, setToken] = useState<string | null>(null);
+
+  // Check if there is a token.
+  // If there is not a token, we want to not show the page
+  // When this page loads, we want to check for that token
+
+  useEffect(() => {
+    // We want to write our logic to check if there is a token
+    const tokenFromBrowser = localStorage.getItem("token");
+    setToken(tokenFromBrowser);
+  }, []);
+
+  if (token === null) {
+    return (
+      <div>
+        <Layout>
+          <h1>🔒 NOT LOGGED IN!!</h1>
+          <Link href="/login">Login Here</Link>
+        </Layout>
+      </div>
+    );
+  } else {
+    return <div>{children}</div>;
+  }
 };
 
 export default ProtectedPage;
