4be5971f35d6ae788bcb888c0300073434431341
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-06 21:04:46 +0100

    crreated form login with useForm

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,23 @@
 import Layout from "@/components/Layout";
 import { useRouter } from "next/router";
-import BetterButton from "../components/BetterButton";
+import { useForm } from "react-hook-form";
+
+interface FormValues {
+  username: string;
+  password: string;
+}
 
 const Login = () => {
   const router = useRouter();
 
-  const handleLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const handleLogin = (data: FormValues) => {
+    console.log(data);
     // Log the user in
     const token = "fake-token"; // This would normally come from the server
     localStorage.setItem("token", token);
@@ -15,9 +27,25 @@ const Login = () => {
   return (
     <Layout>
       <h1>Login</h1>
-      <BetterButton onClick={handleLogin} variant="primary">
-        Login
-      </BetterButton>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <div>
+          <label>
+            Username
+            <input id="username" {...register("username")}></input>
+            {errors.username && errors.username?.message}
+          </label>
+          <label>
+            Password
+            <input
+              id="password"
+              type="password"
+              {...register("password", { required: true })}
+            ></input>
+            {errors.password && errors.password?.message}
+          </label>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </Layout>
   );
 };
