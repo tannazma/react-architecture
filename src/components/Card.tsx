fe7a36b488950fe9455149147d4449ea063c5f8e
tannazma <97254921+tannazma@users.noreply.github.com>
2023-12-06 14:13:07 +0100

    created Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        architecto, praesentium aliquam quibusdam facilis nam omnis, modi vitae
+        aliquid corporis repellendus vel libero enim vero expedita hic illo
+        voluptate pariatur.
+      </p>
+    </div>
+  );
+};
+export default Card;
